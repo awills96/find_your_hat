ef390a493a3d3251c1ee49f13bff15ffd3a13d3f
awills96 <vtalex2014@gmail.com>
2022-05-02 17:06:26 -0400

    added movement and such

--- main.js
+++ main.js
@@ -7,8 +7,14 @@ const pathCharacter = '*';
 
 
 class Field {
+
+    _playerX;
+    _playerY;
+
     constructor(field) {
         this._field = field;
+        this._playerX = 0;
+        this._playerY = 0;
     }
 
     print() {
@@ -18,8 +24,66 @@ class Field {
             }
             process.stdout.write('\n');
         }
+    }
 
+    askInput(){
+        this.print();
+        const input = prompt('Which way? ');
+        switch (input) {
+            case 'u':
+                this._playerY -= 1;
+            break;
+            case 'd':
+                this._playerY += 1;
+            break;
+            case 'l':
+                this._playerX -= 1;
+            break;
+            case 'r':
+                this._playerX += 1;
+            break;
+            default:
+                console.log('Invalid Input. Choose u d l r')
+        }
+
+    }
+
+    testWinLoss(){
+
+        //console.log(`player x is ${this._playerX}, player y is ${this._playerY}`)
+        if (this._playerX >= this._field[0].length || this._playerX < 0) {
+            console.log('Out of bounds!')
+            return true;
+        }
+
+        else if (this._playerY >= this._field.length || this._playerY < 0) {
+            console.log('Out of bounds!')
+            return true;
+        }
+
+        else if(this._field[this._playerY][this._playerX] == 'O'){
+            console.log('You fell into a hole!')
+            return true;
+        }
         
+        else if(this._field[this._playerY][this._playerX] == '^'){
+            console.log('You found the hat! Nice!')
+            return true;
+        }
+
+        else {
+            this._field[this._playerY][this._playerX] = '*';
+            return false;
+        }
+    }
+
+    playGame(){
+        let end = false;
+
+        while (!end){
+            this.askInput();
+            end = this.testWinLoss();
+        }
     }
 }
 
@@ -29,4 +93,4 @@ const myField = new Field([
     ['░', '^', '░'],
   ]);
 
-  myField.print();
+  myField.playGame();
