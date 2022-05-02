19a61d9a2a6d12e54386ae3f59d223e39a8eae98
awills96 <vtalex2014@gmail.com>
2022-05-01 20:26:58 -0400

    started implementation

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+
+    print() {
+        for (let i = 0; i < this._field.length; i++) {
+            for (let k = 0; k < this._field[i].length; k++) {
+                process.stdout.write(this._field[i][k]);
+            }
+            process.stdout.write('\n');
+        }
+
+        
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+  myField.print();
